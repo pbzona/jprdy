7e4bd46f4548baa16e820f4e7f2901283c877f04
Phil Zona <phil.b.zona@gmail.com>
2018-05-31 15:03:37 -0500

    Wrap ScoreButton component in li instead of div

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
-});
+const expect = require('chai').expect;
+
+// describe('Rendering', () => {
+// 	it('renders without crashing', () => {
+// 		const div = document.createElement('div');
+// 		ReactDOM.render(<App />, div);
+// 	});
+// });
+
+global.window = {};
+// import localStorage from 'mock-local-storage';
+let localStorage = require('mock-local-storage');
+window.localStorage = global.localStorage;
+
+const initState = {
+	buttonValues: [200, 400, 600, 800, 1000],
+	activeValue: 0,
+	players: [],
+	addingPlayer: false,
+	round: 1,
+	playerScores: [],
+	isWagering: 0,
+	final: {
+		wagers: {},
+		answers: {}
+	},
+	gameInProgress: false
+};
+
+// describe('App state', () => {
+// 	before(() => {
+// 		localStorage = initState;
+// 	});
+
+// 	it('should have a state with the proper keys', () => {
+// 		const keys = Object(localStorage).keys
+// 		expect(keys).to.equal(Object.keys(initState));
+// 	});
+// });
+
+localStorage = initState;
+console.log(JSON.stringify(localStorage));
+
--- src/components/ScoreButton.js
+++ src/components/ScoreButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const ScoreButton = ({ onSelectValue, value, buttonKey }) => {
 	return (
-		<div className="score__container">
+		<li className="score__container">
 			<button
 				className={`score__button button-${buttonKey}`}
 				onClick={() => onSelectValue(value, buttonKey)}
 			>
 				${value}
 			</button>
-		</div>
+		</li>
 	);
 };
 
